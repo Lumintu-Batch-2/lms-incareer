398af4ab30fbf1b3fbbd1bd682884d54618075b1
Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>
2022-04-09 20:49:30 +0700

    Added role mentor functionality
    Signed-off-by: Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>

--- incareer2.sql
+++ incareer2.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Apr 05, 2022 at 05:26 AM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
+-- Host: 127.0.0.1
+-- Generation Time: Apr 08, 2022 at 08:05 AM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -36,6 +36,14 @@ CREATE TABLE `assignments` (
   `subject_id` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `assignments`
+--
+
+INSERT INTO `assignments` (`assignment_id`, `assignment_name`, `assignment_start_date`, `assignment_end_date`, `assignment_desc`, `subject_id`) VALUES
+(2, 'Membuat List', '2022-04-07 09:27:18', '2022-04-15 14:27:18', 'nmgkjxu saiuiu dcgudgdiug ', 3),
+(3, 'Membuat Linked List', '2022-04-07 09:27:18', '2022-04-15 14:27:18', 'sjkdh sdsoi osi oso', 4);
+
 -- --------------------------------------------------------
 
 --
@@ -46,8 +54,15 @@ CREATE TABLE `assignment_questions` (
   `assignment_question_id` int(10) UNSIGNED NOT NULL,
   `question_filename` varchar(255) NOT NULL,
   `question_upload_date` datetime NOT NULL,
-  `assignment_id` int(10) UNSIGNED NOT NULL
+  `assignment_id` int(10) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `assignment_questions`
+--
+
+INSERT INTO `assignment_questions` (`assignment_question_id`, `question_filename`, `question_upload_date`, `assignment_id`) VALUES
+(1, 'Jadwal Latihan FG.xlsx', '2022-04-06 13:06:44', NULL);
 
 -- --------------------------------------------------------
 
@@ -59,7 +74,7 @@ CREATE TABLE `assignment_submissions` (
   `assignment_submission_id` int(10) UNSIGNED NOT NULL,
   `submission_filename` varchar(255) NOT NULL,
   `submitted_date` datetime NOT NULL,
-  `assignment_id` int(10) UNSIGNED NOT NULL
+  `assignment_id` int(10) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -74,6 +89,16 @@ CREATE TABLE `courses` (
   `course_desc` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`course_id`, `course_name`, `course_desc`) VALUES
+(1, 'Algoritma Struktur Data', 'sjgfdkafjdkvbdigfiowebfvkjdubcvfdv fukdnoigdvsdvdv'),
+(2, 'Database Development', 'safkhsafmwpbonut9w0 Y E8FRYE PIFEFYEWP UGUEIWF J\'iyg[9aer yioew fuopeu y ye9gywei fhioqe foiwe'),
+(3, 'Algoritma Pemrograman', 'kjshfiue  ureiuy ey ey uyweiu yifiufgkjfhkfjh euyruew iueriu e giuegiehiuiufueif   ufudhfkjdfdhfkhgiguriugyrruiywy;a;ggkdfj'),
+(4, 'Matematika Diskret', 'xlkjfdiorjew eio jioerurioe o oieewoi dkjhfkjdhj oihfld hodhforywrojgio o ohohfsdkjhvkjfshvjk ds hfodsh guirhfogi wo hwhwuih ho   hofhfhdllsdd sfhf  hldsllksfslk j   flkjljsfjfslkd    sfjldjldfl');
+
 -- --------------------------------------------------------
 
 --
@@ -86,6 +111,20 @@ CREATE TABLE `subjects` (
   `subject_desc` text NOT NULL,
   `course_id` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `subjects`
+--
+
+INSERT INTO `subjects` (`subject_id`, `subject_name`, `subject_desc`, `course_id`) VALUES
+(3, 'List', 'akldfkldajf dlkanflkdanfld klabdfljd', 1),
+(4, 'Linked List', 'klafhkldhf\'odgfodnf', 1),
+(5, 'Join', ',msfhkjdhfdahd hkf hdahd', 2),
+(6, 'Mysql', 'dklfjldf dhodivjoid v vidpo d', 2),
+(7, 'Tipe Data', 'safnkj;dfkdlnvkdn lkd jvioduvpods', 3),
+(8, 'Dictionary', 'xmcndlkv dhvoidsj;lds pidupodsjsdpov udvpoW', 3),
+(9, 'Matematika', 'mdnvlkd dihidslv dgvd\'ovhds\' vds', 4),
+(10, 'Diskret', 'm,dnvlk dhiofh ldskhd vhdvhdjkv djv hduh dljkshlkshf lfshjvlhj vkvkjhvkjshvkjshs hvfkhfukhfuk ', 4);
 
 -- --------------------------------------------------------
 
@@ -100,6 +139,14 @@ CREATE TABLE `users` (
   `role` tinyint(4) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `password`, `role`) VALUES
+(5, 'Andy', '1234', 1),
+(6, 'Ilham', '1234', 2);
+
 -- --------------------------------------------------------
 
 --
@@ -111,6 +158,28 @@ CREATE TABLE `user_assignments` (
   `user_id` int(10) UNSIGNED NOT NULL,
   `assignment_id` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_courses`
+--
+
+CREATE TABLE `user_courses` (
+  `user_course_id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `course_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_courses`
+--
+
+INSERT INTO `user_courses` (`user_course_id`, `user_id`, `course_id`) VALUES
+(1, 5, 1),
+(2, 5, 2),
+(3, 6, 1),
+(4, 6, 2);
 
 --
 -- Indexes for dumped tables
@@ -165,6 +234,14 @@ ALTER TABLE `user_assignments`
   ADD KEY `fk_users` (`user_id`);
 
 --
+-- Indexes for table `user_courses`
+--
+ALTER TABLE `user_courses`
+  ADD PRIMARY KEY (`user_course_id`),
+  ADD KEY `fk_user_id` (`user_id`),
+  ADD KEY `fk_course_id` (`course_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -172,43 +249,49 @@ ALTER TABLE `user_assignments`
 -- AUTO_INCREMENT for table `assignments`
 --
 ALTER TABLE `assignments`
-  MODIFY `assignment_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `assignment_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `assignment_questions`
 --
 ALTER TABLE `assignment_questions`
-  MODIFY `assignment_question_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `assignment_question_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `assignment_submissions`
 --
 ALTER TABLE `assignment_submissions`
-  MODIFY `assignment_submission_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `assignment_submission_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT for table `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `course_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `course_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `subjects`
 --
 ALTER TABLE `subjects`
-  MODIFY `subject_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `subject_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `user_assignments`
 --
 ALTER TABLE `user_assignments`
   MODIFY `user_assigment_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_courses`
+--
+ALTER TABLE `user_courses`
+  MODIFY `user_course_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
@@ -244,6 +327,13 @@ ALTER TABLE `subjects`
 ALTER TABLE `user_assignments`
   ADD CONSTRAINT `fk_assignments` FOREIGN KEY (`assignment_id`) REFERENCES `assignments` (`assignment_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `fk_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user_courses`
+--
+ALTER TABLE `user_courses`
+  ADD CONSTRAINT `fk_course_id` FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
