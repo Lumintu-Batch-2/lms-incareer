b96a2ab0995bd95241bf9ee45115d2ba20337d41
Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>
2022-04-20 16:40:20 +0700

    Fix file upload_submission
    Signed-off-by: Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>

--- Role/Student/upload_submission.php
+++ Role/Student/upload_submission.php
@@ -2,53 +2,59 @@
 
 print_r($_FILES);
 // print_r($_POST['submission_id']);
+$path = '../../Upload/Assignment/Submission/';
+$move = move_uploaded_file($_FILES['data']['tmp_name'], $path . $_FILES['data']['name']);
 
-require_once "../../Model/AssignmentSubmission.php";
-$objAssig = new AssignmentSubmission;
-date_default_timezone_set("Asia/Jakarta");
-$dateupload = date("Y-m-d H:i:s");
+if($move) {
 
-$is_ok = false;
-$msg = "";
+    require_once "../../Model/AssignmentSubmission.php";
+    $objAssig = new AssignmentSubmission;
+    date_default_timezone_set("Asia/Jakarta");
+    $dateupload = date("Y-m-d H:i:s");
 
-$validExtention = ['pdf', 'doc', 'docx', 'xlsx', 'txt', 'png', 'jpg', 'jpeg', 'ppt', 'pdf', 'rar', 'zip'];
-$fileExtention = explode(".", $_FILES['data']['name']);
-$fileExtention = strtolower(end($fileExtention));
-$filesize = $_FILES['data']['size'];
-if (!in_array($fileExtention, $validExtention)) {
-    $msg = "Format file tidak didukung!";
-    goto out;
-}
+    $is_ok = false;
+    $msg = "";
 
-// if ($dateupload < $assignment['assignment_start_date']) {
-//     $msg = "Assignment Belum Dimulai";
-//     goto out;
-// }
-// if ($dateupload > $assignment['assignment_end_date']) {
-//     $msg = "Assignment sudah melebihi deadline";
-//     goto out;
-// }
+    $validExtention = ['pdf', 'doc', 'docx', 'xlsx', 'txt', 'png', 'jpg', 'jpeg', 'ppt', 'pdf', 'rar', 'zip'];
+    $fileExtention = explode(".", $_FILES['data']['name']);
+    $fileExtention = strtolower(end($fileExtention));
+    $filesize = $_FILES['data']['size'];
+    if (!in_array($fileExtention, $validExtention)) {
+        $msg = "Format file tidak didukung!";
+        goto out;
+    }
 
-if ($filesize > 2000000) {
-    $msg = "Ukuran file tidak boleh lebih dari 2 Mb";
-    var_dump($filesize);
-    goto out;
-}
+    // if ($dateupload < $assignment['assignment_start_date']) {
+    //     $msg = "Assignment Belum Dimulai";
+    //     goto out;
+    // }
+    // if ($dateupload > $assignment['assignment_end_date']) {
+    //     $msg = "Assignment sudah melebihi deadline";
+    //     goto out;
+    // }
 
-$objAssig->setSubmissionFileName($_FILES['data']['name']);
-$objAssig->setSubmissionUploadDate($dateupload);
-$objAssig->setAssignmentSubmissionId((int) $_POST['submission_id']);
+    if ($filesize > 2000000) {
+        $msg = "Ukuran file tidak boleh lebih dari 2 Mb";
+        var_dump($filesize);
+        goto out;
+    }
 
-$update = $objAssig->updateSubmission();
+    $objAssig->setSubmissionFileName($_FILES['data']['name']);
+    $objAssig->setSubmissionUploadDate($dateupload);
+    $objAssig->setAssignmentSubmissionId((int) $_POST['submission_id']);
 
-if($update) {
+    $update = $objAssig->updateSubmission();
 
-    $path = '../../Upload/Assignment/Submission/';
-    $move = move_uploaded_file($_FILES['data']['tmp_name'], $path . $_FILES['data']['name']);
+    if($update) {
+        $msg = "Berhasil mengirim tugas!";
+        $is_ok = true;
+        goto out;
+    } else {
+        $msg = "Gagal mengirim tugas!";
+        goto out;
+    }
 
-    $msg = "Berhasil mengirim tugas!";
-    $is_ok = true;
-    goto out;
+    
 }
 
 out: {
