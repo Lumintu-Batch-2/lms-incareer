90f5c8b404bfe9ab655ffdb662c7b63ad3ae5839
L200180194 <alfiantoandy6@gmail.com>
2022-05-05 21:56:13 +0700

    disable button

--- Role/Student/assignment.php
+++ Role/Student/assignment.php
@@ -60,8 +60,8 @@ for ($i = 0; $i < count($modulJSON->{'data'}); $i++) {
 
 $subModul = array();
 
-for($i = 0; $i < count($subModulData->{'data'}); $i++) {
-    if($subModulData->{'data'}[$i]->{'id'} == $_GET['subject_id']) {
+for ($i = 0; $i < count($subModulData->{'data'}); $i++) {
+    if ($subModulData->{'data'}[$i]->{'id'} == $_GET['subject_id']) {
         array_push($subModul, $subModulData->{'data'}[$i]);
     }
 }
@@ -274,7 +274,7 @@ echo "<input type='hidden' id='student_id' value='" . $_SESSION['user']->{'user_
             <div class="flex items-center gap-x-4 w-full bg-white py-4 px-10 rounded-xl">
                 <img class="w-14" src="../../Img/icons/default_profile.svg" alt="Profile Image">
                 <div class="">
-                    <p class="text-dark-green text-base font-semibold"><?= $lectureData[0]->{'user_first_name'} . " " . $lectureData[0]->{'user_last_name'}?> | Mentor Code</p>
+                    <p class="text-dark-green text-base font-semibold"><?= $lectureData[0]->{'user_first_name'} . " " . $lectureData[0]->{'user_last_name'} ?> | Mentor Code</p>
                     <p class="text-light-green">Mentor Specialization</p>
                 </div>
             </div>
@@ -301,10 +301,11 @@ echo "<input type='hidden' id='student_id' value='" . $_SESSION['user']->{'user_
             <div>
                 <table class="shadow-lg bg-white" style="width: 100%">
                     <colgroup>
-                        <col span="1" style="width: 20%">
+                        <col span="1" style="width: 15%">
                         <col span="1" style="width: 10%">
                         <col span="1" style="width: 10%">
                         <col span="1" style="width: 10%">
+                        <col span="1" style="width: 5%">
                         <col span="1" style="width: 10%">
                         <col span="1" style="width: 10%">
                         <col span="1" style="width: 10%">
@@ -315,6 +316,7 @@ echo "<input type='hidden' id='student_id' value='" . $_SESSION['user']->{'user_
                             <th class="border-b text-center px-4 py-2">Start Date</th>
                             <th class="border-b text-center px-4 py-2">Due Date</th>
                             <th class="border-b text-center px-4 py-2">Due Time</th>
+                            <th class="border-b text-center px-4 py-2">Description</th>
                             <th class="border-b text-center px-4 py-2">Questions</th>
                             <th class="border-b text-center px-4 py-2">Upload</th>
                             <th class="border-b text-center px-4 py-2">Assignment History</th>
@@ -333,9 +335,11 @@ echo "<input type='hidden' id='student_id' value='" . $_SESSION['user']->{'user_
                             ?>
                             <tr>
                                 <td class="border-b px-4 py-2"><?= $assignment['assignment_name']; ?></td>
-                                <td class="border-b px-4 py-2"><?= $startDate; ?></td>
-                                <td class="border-b px-4 py-2"><?= $dueDate; ?></td>
-                                <td class="border-b px-4 py-2"><?= $dueTime; ?></td>
+                                <td class="border-b px-4 py-2 text-center"><?= $startDate; ?></td>
+                                <td class="border-b px-4 py-2 text-center"><?= $dueDate; ?></td>
+                                <td class="border-b px-4 py-2 text-center"><?= $dueTime; ?></td>
+                                <td class="border-b px-4 py-2 text-center"><a href="#"><img class="w-7 mx-auto cursor-pointer" src="../../Img/icons/detail_icon.svg" alt="Download Icon" type="button" data-modal-toggle="medium-modal<?= "medium-modal" . $assignment['assignment_id'] ?>" id="showDesc" data-desc="<?= $assignment['assignment_desc'] ?>"></a></td>
+
                                 <td class="border-b px-4 py-2">
                                     <?php
                                     require_once('../../Model/AssignmentQuestion.php');
@@ -345,7 +349,45 @@ echo "<input type='hidden' id='student_id' value='" . $_SESSION['user']->{'user_
                                     ?>
                                     <a href="download.php?file=<?= $question['question_filename'] . '&type=q'; ?>"><img class=" w-7 mx-auto cursor-pointer" src="../../Img/icons/download_icon.svg" alt="Download Icon"></a>
                                 </td>
-                                <td class="border-b px-4 py-2"><img class="w-7 mx-auto cursor-pointer modalUpload" src="../../Img/icons/create_icon.svg" alt="Create Icon" type="button" data-modal-toggle="defaultModal<?= $assignment['assignment_id']; ?>" data-assignid="<?= $assignment['assignment_id']; ?>" id="uploadModal"></td>
+                                <td class="border-b px-4 py-2">
+                                    <?php
+                                    date_default_timezone_set('Asia/Jakarta');
+                                    $now =  date("Y-m-d h:i:s");
+                                    require_once "../../Model/AssignmentSubmission.php";
+                                    $objsubmit = new AssignmentSubmission;
+                                    $objsubmit->setStudentId($_SESSION['user']->{'user_id'});
+                                    $objsubmit->setAssignmentId($assignment['assignment_id']);
+                                    $csub = $objsubmit->getSubmissionByAssignIdAndStudentIdGroupBy();
+
+
+
+                                    if (strtotime($now) >= strtotime($assignment['assignment_end_date'])) {
+
+                                    ?>
+
+                                        <img class="w-7 mx-auto " data-tooltip-target="tooltip-default" src="../../Img/icons/create_iconred.svg" alt="Create Icon" name="btnup" id="btnup">
+                                        <div id="tooltip-default" role="tooltip" class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-red-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-red-700">
+                                            You missed the deadline !!
+                                            <div class="tooltip-arrow" data-popper-arrow></div>
+                                        </div>
+
+                                    <?php
+                                    } elseif (count($csub) >= 3) {
+                                    ?>
+                                        <img class="w-7 mx-auto " data-tooltip-target="tooltip-default1" src="../../Img/icons/create_iconred.svg" alt="Create Icon" name="btnup" id="btnup">
+                                        <div id="tooltip-default1" role="tooltip" class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-red-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-red-700">
+                                            You've already done the third attempt !!
+                                            <div class="tooltip-arrow" data-popper-arrow></div>
+                                        </div>
+                                    <?php
+                                    } else {
+
+                                    ?>
+                                        <img class="w-7 mx-auto cursor-pointer modalUpload" src="../../Img/icons/create_icon.svg" alt="Create Icon" type="button" data-modal-toggle="defaultModal<?= $assignment['assignment_id']; ?>" data-assignid="<?= $assignment['assignment_id']; ?>" id="uploadModal">
+                                    <?php
+                                    }
+                                    ?>
+                                </td>
                                 <td class="border-b px-4 py-2"><img class="w-7 mx-auto cursor-pointer" src="../../Img/icons/history_icon.svg" alt="History Icon" type="button" data-modal-toggle="historymodal<?= $assignment['assignment_id']; ?>">
                                 </td>
 
@@ -372,32 +414,32 @@ echo "<input type='hidden' id='student_id' value='" . $_SESSION['user']->{'user_
                                                         <li><b>Files</b></li>
                                                     </ul>
                                                     <?php
-                                                        require_once "../../Model/AssignmentSubmission.php";
-                                                        $objSub = new AssignmentSubmission;
-                                                        $objSub->setStudentId($_SESSION['user']->{'user_id'});
-                                                        $objSub->setAssignmentId($assignment['assignment_id']);
-                                                        $submissions = $objSub->getSubmissionByAssignIdAndStudentId();
+                                                    require_once "../../Model/AssignmentSubmission.php";
+                                                    $objSub = new AssignmentSubmission;
+                                                    $objSub->setStudentId($_SESSION['user']->{'user_id'});
+                                                    $objSub->setAssignmentId($assignment['assignment_id']);
+                                                    $submissions = $objSub->getSubmissionByAssignIdAndStudentId();
 
-                                                        $i = 1;
+                                                    $i = 1;
 
-                                                        foreach($submissions as $val => $submission) :
+                                                    foreach ($submissions as $val => $submission) :
                                                     ?>
-                                                    <ul class="grid grid-cols-12 border-b border-gray-400 py-2">
-                                                        <li><?= $i; ?></li>
-                                                        <li class="col-span-5"><?=$submission['submitted_date'];?></li>
-                                                        <li class="col-span-5 truncate"><?= $submission['submission_filename'];?></li>
-                                                        <li><a href="download.php?file=<?= $submission['submission_filename'] . '&type=s'; ?>"><img class=" w-7 mx-auto cursor-pointer" src="../../Img/icons/download_icon.svg" alt="Download Icon"></a></li>
-                                                    </ul>
+                                                        <ul class="grid grid-cols-12 border-b border-gray-400 py-2">
+                                                            <li><?= $i; ?></li>
+                                                            <li class="col-span-5"><?= $submission['submitted_date']; ?></li>
+                                                            <li class="col-span-5 truncate"><?= $submission['submission_filename']; ?></li>
+                                                            <li><a href="download.php?file=<?= $submission['submission_filename'] . '&type=s'; ?>"><img class=" w-7 mx-auto cursor-pointer" src="../../Img/icons/download_icon.svg" alt="Download Icon"></a></li>
+                                                        </ul>
 
-                                                    <?php 
+                                                    <?php
                                                         $i++;
-                                                        endforeach     
+                                                    endforeach
                                                     ?>
-                                                    
+
                                                 </div>
                                             </div>
                                             <div class="flex justify-end p-6 space-x-3 rounded-b ">
-                                                <button data-modal-toggle="historymodal<?= $assignment['assignment_id']; ?>" class="w-32 bg-yellow-400 text-center py-1 text-white rounded-md hover:bg-yellow-600" type="button">Confirm</button>
+                                                <button data-modal-toggle="historymodal<?= $assignment['assignment_id']; ?>" class="w-32 bg-yellow-400 text-center py-1 text-white rounded-md hover:bg-yellow-600" type="button">Close</button>
                                             </div>
 
                                         </div>
@@ -453,6 +495,36 @@ echo "<input type='hidden' id='student_id' value='" . $_SESSION['user']->{'user_
                                 </div>
                             </div>
                             <!-- END MODAL -->
+                            <!-- Description Modal -->
+                            <div id="medium-modal<?= "medium-modal" . $assignment['assignment_id'] ?>" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
+                                <div class="relative p-4 w-full max-w-lg h-full md:h-auto">
+                                    <!-- Modal content -->
+                                    <div class="relative bg-white rounded-lg shadow ">
+                                        <!-- Modal header -->
+                                        <div class="flex justify-between items-center p-5 rounded-t border-b dark:border-gray-600">
+                                            <h3 class="text-xl font-medium text-center">
+                                                Description
+                                            </h3>
+                                            <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="medium-modal<?= "medium-modal" . $assignment['assignment_id'] ?>">
+                                                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                                                </svg>
+                                            </button>
+                                        </div>
+                                        <!-- Modal body -->
+                                        <div class="p-6 space-y-6">
+                                            <p class="text-base leading-relaxed">
+                                                <?= $assignment['assignment_desc'] ?>
+                                            </p>
+                                        </div>
+                                        <!-- Modal footer -->
+                                        <div class="flex justify-end p-6 space-x-2 rounded-b border-gray-200 dark:border-gray-600">
+                                            <button data-modal-toggle="medium-modal<?= "medium-modal" . $assignment['assignment_id'] ?>" type="button" class="text-gray bg-cream focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-transparent hover:text-white hover:bg-gray-600 dark:focus:ring-dark-800">Close</button>
+                                            <!-- <button data-modal-toggle="medium-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button> -->
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         <?php endforeach ?>
                     </tbody>
                 </table>
@@ -494,7 +566,7 @@ echo "<input type='hidden' id='student_id' value='" . $_SESSION['user']->{'user_
                 let student_id = studentId.value;
                 let fileData = document.getElementById("fileInput");
 
-                
+
                 $(document).on("click", "#uploadSubmission", function(evt) {
                     evt.preventDefault();
                     let cf = document.getElementById("cf");
